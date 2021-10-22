4e8ec83f4f29cf33a25644709649bab9368cff8d
Katarzyna Kuich <neokatrina@gmail.com>
2021-10-22 18:30:13 +0200

    Init project

---
+++ main.py
@@ -0,0 +1,10 @@
+#kodilla zadanie Katarzyna Kuich
+#Lista zakupów
+#Idę do Piekarnia, kupuję tu następujące rzeczy: ['Chleb', 'Pączek', 'Bułki'].
+#Idę do Warzywniak, kupuję tu następujące rzeczy: ['Marchew', 'Seler', 'Rukola'].
+#W sumie kupuję 6 produktów.
+
+print("\nZadanie 1")
+print("Lista zakupow:")
+
+
